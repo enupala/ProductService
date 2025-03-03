7e0a8ea1d4c0cd08f36c264670341663a967cebf
Kalyani.Enupala <enupalakalyani123@gmail.com>
2025-03-03 15:13:29 +0530

    Configured Load Balancer

--- src/main/java/com/ecommerce/ProductService/config/ProdConfig.java
+++ src/main/java/com/ecommerce/ProductService/config/ProdConfig.java
@@ -1,12 +1,15 @@
 package com.ecommerce.ProductService.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 @Component
 public class ProdConfig {
+
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate(){
         return new RestTemplate();
     }
